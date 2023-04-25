c46af5e7d1abdc3d0cf977a3158bb82f34ba892f
chaurea <amol.chaure@sincrodigital.com>
2023-04-25 19:22:46 +0530

    Script updateto stop

--- stop.sh
+++ stop.sh
@@ -1,12 +1,12 @@
 #To remove spring boot application image
 removeSpringAppImage() {
+  docker rm $1
   docker rmi $1
   rm -rf $1/target
 }
 
 docker-compose stop
 docker rm demo-rabbit
-docker rm api
 
 #removeSpringAppImage 'demo-producer'
 #removeSpringAppImage 'demo-consumer'
